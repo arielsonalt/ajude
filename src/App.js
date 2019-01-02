c2d85408d0f20fcdd580dc874e29eab589208390
arielsonalt <32400603+arielsonalt@users.noreply.github.com>
2019-01-02 19:22:35 -0200

    Atualização de Login e Home

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Router } from 'react-router-dom';
+
+import Home from './component/Home'
+import Login from './component/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <BrowserRouter>
+        <Router path='/' component={Home} />
+        <Router path='/Login' component={Login} />
+      </BrowserRouter>
+    )
   }
 }
-
-export default App;
