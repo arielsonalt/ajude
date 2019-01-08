22a5e193709c4212df4dd82954745ef5de567805
arielsonalt <32400603+arielsonalt@users.noreply.github.com>
2019-01-08 10:06:47 -0200

    Modularização do rodapé: implementação do Footer.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { ResponsiveContainer } from './navbar/Navbar'
 import imagem from '../img/land.jpg'
+import Footer from './footer/Footer'
 import {
     Button,
     Container,
@@ -135,40 +136,7 @@ const HomepageLayout = () => (
         </Button>
             </Container>
         </Segment>
-        <Segment inverted color="blue" vertical style={{ padding: '5em 0em' }}>
-            <Container>
-                <Grid divided inverted stackable>
-                    <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='About' />
-                            <List link inverted>
-                                <List.Item as='a'>Sitemap</List.Item>
-                                <List.Item as='a'>Contact Us</List.Item>
-                                <List.Item as='a'>Religious Ceremonies</List.Item>
-                                <List.Item as='a'>Gazebo Plans</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Services' />
-                            <List link inverted>
-                                <List.Item as='a'>Banana Pre-Order</List.Item>
-                                <List.Item as='a'>DNA FAQ</List.Item>
-                                <List.Item as='a'>How To Access</List.Item>
-                                <List.Item as='a'>Favorite X-Men</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
-                            <Header as='h4' inverted>
-                                Footer Header
-              </Header>
-                            <p>
-                                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
-        </Segment>
+        <Footer />
     </div>
 );
 export default HomepageLayout
