3d97226ab33a63c6576293cc9a3226e6631f5950
arielsonalt <32400603+arielsonalt@users.noreply.github.com>
2019-01-10 19:47:42 -0200

    Implementação das telas de cadastro de Aluno, funcionário e servidor

--- src/components/cadastro/CadastroFuncionario.js
+++ src/components/cadastro/CadastroFuncionario.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react'
-import { Container, Segment, Header } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import {
+    Segment,
+    Header,
+    Grid,
+    Form,
+    Message,
+    Button
+} from 'semantic-ui-react'
 
 export class CadastroFuncionario extends Component {
     render() {
         return (
 
-            <Container style={{ backgrond: 'blue', minWidght: "100%" }}>
 
-                <Segment
-                    inverted
-                    color='blue'
-                >
-                    <Header></Header>
-                </Segment>
-
-            </Container>
-
+            <div className='login-form'>
+                <style>
+                    {
+                        `body > div,
+                    body > div > div,
+                    body > div > div > div.login-form {
+                                                        height: 50.5em;
+                                                        background:#2185d0!important;
+                                                        }`
+                    }
+                </style>
+                <Grid textAlign='center' style={{ height: '100%', padding: '2em' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as='h2' inverted textAlign='center'>
+                            Cadastro de Funcionário
+                        </Header>
+                        <Form>
+                            <Segment stacked textAlign="left">
+                                <Form.Field>
+                                    <Form.Input fluid icon='user' iconPosition='left' label='Nome' placeholder='Nome' name='nome' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Form.Input label='Email' placeholder='E-mail' name='email' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Form.Input label='Confirme o email' placeholder='E-mail' name='confirmaEmail' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Form.Input label='Senha' placeholder='Senha' name='senha' />
+                                </Form.Field>
+                                <Form.Field>
+                                    <Form.Input label='Confirme a senha' placeholder='Senha' name='confimaSenha' />
+                                </Form.Field>
+                                <Button color='green' fluid size='large'>
+                                    Salvar
+                                    </Button>
+                            </Segment>
+                        </Form>
+                        <Message>
+                            <Link to='/'>
+                                <Button color='red' fluid size='large'>
+                                    Cancelar
+                            </Button>
+                            </Link>
+                        </Message>
+                    </Grid.Column>
+                </Grid>
+            </div>
         );
     }
 }
